69885550a6acfff2aef7708edcb837e04fa1e57d
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-14 17:41:22 -0700

    recover changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import List from './list';
+import SingleItem from './single_item';
 import 'materialize-css/dist/css/materialize.min.css';
 
 const App = () => (
     <div>
         <div className="container">
-        <h1 className="center">To Do List</h1>
-        <List />
+            <h1 className="center">To Do List</h1>
+            <List />
         </div>
     </div>
 );
---
+++ src/components/single_item.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="center">All The Single Items</h1>
+            </div>
+        )
+    }
+}
+
+export default SingleItem;
